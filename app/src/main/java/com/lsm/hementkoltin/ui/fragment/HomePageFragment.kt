40ea0d0b8d465d81f4d83981d5b7a7013e605ac5
shiming.li <lamshiming@sina.com>
2019-08-22 19:10:17 +0800

    1、首页

--- app/src/main/java/com/lsm/hementkoltin/ui/fragment/HomePageFragment.kt
+++ app/src/main/java/com/lsm/hementkoltin/ui/fragment/HomePageFragment.kt
@@ -1,10 +1,17 @@
 package com.lsm.hementkoltin.ui.fragment
 
+import android.content.Context
+import android.hardware.Camera
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraManager
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.LifecycleObserver
 import com.lsm.base.weight.MultipleStatusView
-import com.lsm.hementkoltin.R
+import kotlinx.android.synthetic.main.fragment_tab_first.*
 import kotlinx.android.synthetic.main.toolbar.*
+
 
 /**
  * <p>
@@ -15,9 +22,13 @@ import kotlinx.android.synthetic.main.toolbar.*
  * @version v1.0
  * @since 2019/8/21 10:13
  */
-class HomePageFragment :BaseMainFragment(){
+class HomePageFragment : BaseMainFragment(), View.OnClickListener {
 
-    companion object{
+
+
+    private var light = false
+
+    companion object {
 
         fun newInstance(): HomePageFragment {
             val args = Bundle()
@@ -30,6 +41,8 @@ class HomePageFragment :BaseMainFragment(){
 
     override fun initListener() {
         mToolBar.title = "HomePageFragment"
+        light_turn_off.setOnClickListener(this)
+        light_turn_btn.setOnClickListener(this)
     }
 
     override fun getPresenter(): LifecycleObserver? {
@@ -40,10 +53,70 @@ class HomePageFragment :BaseMainFragment(){
         return null
     }
 
-    override fun getLayoutId(): Int = R.layout.fragment_tab_first
+    override fun getLayoutId(): Int = com.lsm.hementkoltin.R.layout.fragment_tab_first
 
     override fun lazyLoad() {
 
     }
 
+    override fun onClick(p0: View?) {
+        when (p0?.id) {
+            com.lsm.hementkoltin.R.id.light_turn_off -> {
+
+            }
+            com.lsm.hementkoltin.R.id.light_turn_btn -> {
+                if (!light) {
+                    light_turn_btn.setImageResource(com.lsm.hementkoltin.R.drawable.turn_on)
+                    light_turn_on.visibility = View.VISIBLE
+                    Thread(Runnable { openFlash() }).start()
+                    light = true
+                } else {
+                    cancel()
+                    light = false
+                }
+            }
+        }
+    }
+
+    private fun openFlash() {
+        var systemService: CameraManager? = null
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            systemService =activity?.getSystemService(Context.CAMERA_SERVICE) as CameraManager?
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    val s = systemService!!.cameraIdList[0]
+                    systemService.setTorchMode(s, true)
+                }
+            } catch (e: CameraAccessException) {
+                e.printStackTrace()
+            }
+
+        }
+
+    }
+    private fun closeFlash() {
+        var systemService: CameraManager? = null
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            systemService = activity?.getSystemService(Context.CAMERA_SERVICE) as CameraManager?
+            try {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    val s = systemService!!.cameraIdList[0]
+                    systemService.setTorchMode(s, false)
+                }
+            } catch (e: CameraAccessException) {
+                e.printStackTrace()
+            }
+
+        }
+    }
+    private fun cancel() {
+        Thread(Runnable { closeFlash() }).start()
+        light = false
+        light_turn_btn.setImageResource(com.lsm.hementkoltin.R.drawable.turn_off)
+        light_turn_on.visibility = View.GONE
+    }
+
+     override fun onDestroy() {
+        super.onDestroy()
+    }
 }
